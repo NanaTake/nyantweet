fcf0660521b1e00b144296a7ceb47143e9ade274
NanaTake <son_magomago@yahoo.co.jp>
2019-06-29 17:38:22 +0900

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root 'tweets#index'
+
+  resources :users, only: :show
+  resources :tweets do
+    resources :comments, only: :create
+  end
+end
